b15f8f8a12a0d219acbf1220063fa8fc51b3d463
Oliver <olimovier@gmail.com>
2020-05-29 17:21:38 +0800

    Rectangle

--- main.js
+++ main.js
@@ -2,10 +2,18 @@
 
 var vertexShaderSource = `#version 300 es
 
-in vec4 a_position;
+in vec2 a_position;
+
+uniform vec2 u_resolution;
 
 void main() {
-  gl_Position = a_position;
+  vec2 zeroToOne = a_position / u_resolution;
+
+  vec2 zeroToTwo = zeroToOne * 2.0;
+
+  vec2 clipSpace = zeroToTwo - 1.0;
+
+  gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
 }
 `;
 
@@ -61,14 +69,18 @@ function main() {
   var program = createProgram(gl, vertexShader, fragmentShader);
 
   var positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+  var resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
 
   var positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
   var positions = [
-    0, 0,
-    0, 0.5,
-    0.7, 0,
+    10, 20,
+    80, 20,
+    10, 30,
+    10, 30,
+    80, 20,
+    80, 30,
   ];
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
@@ -91,11 +103,13 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.useProgram(program);
 
+  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
   gl.bindVertexArray(vao);
 
   var primitiveType = gl.TRIANGLES;
   var offset = 0;
-  var count = 3;
+  var count = 6;
   gl.drawArrays(primitiveType, offset, count);
 }
 
